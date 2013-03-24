e6a08cc595d371eb99287c4b95ca2d97544c4e7b
kalashnikovisme <kalashnikovisme@gmail.com>
2013-03-24 20:51:02 +0400

    add validation to public_work

--- app/models/public_work.rb
+++ app/models/public_work.rb
@@ -2,10 +2,8 @@ class PublicWork < ActiveRecord::Base
   attr_accessible :annotation, :header, :header_coordinates, :nomination, :title, :user_id, :text, :video_link
 
   belongs_to :user
-  validates :annotation, :presence => true
   validates :title, :presence => true
   validates :user_id, :presence => true
   validates :text, :presence => true
-  validates :video_link, :presence => true,
-                         :url => true
+  validates :video_link, :url => { :allow_blank => true, :allow_nil => true }
 end
