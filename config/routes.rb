91982e5835b619140993c35917bf4b3e12cda9af
pavel <kalashnikovisme@gmail.com>
2014-01-27 18:26:47 +0400

    some random adds

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,22 @@
 NLCR::Application.routes.draw do
   root to: "welcome#index"
-
-  get "users/upload_resume"
-
   resources :public_works, only: :index
   resources :users do
     member do
       resources :public_works, except: :index
     end
   end
-
-  match "admin" => "admins#login"
-  match "logout" => "admins#logout"
-  match "upload" => "users#upload_resume"
-  match "upload_r" => "users#upload_r"
-  match "success" => "pages#success"
+  resources :errors, only: [] do
+    collection do
+      get :not_found
+    end
+  end
+  namespace :admin do
+    resources :users do
+      member do
+        put :accept
+        put :reserve
+      end
+    end
+  end
 end
