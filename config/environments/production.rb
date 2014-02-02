16fb97e0f9998d1a929e1580276f500c0ce3aec0
Dmitry Davydov <haudvd@gmail.com>
2014-02-02 09:21:16 +0400

    fix precompile on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ NLCR::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( *.js *.css *.scss *.coffee *.ttf )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
