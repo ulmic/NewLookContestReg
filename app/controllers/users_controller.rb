d60b5f45070a5bca548b53a100916b5bed802b30
pavel <kalashnikovisme@gmail.com>
2014-12-24 00:24:35 +0300

    some fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,6 @@ class UsersController < ApplicationController
   def create
     @user = User.new params[:user]
     if @user.save
-      user_sign_in @user
       redirect_to @user
     else
         render :new
