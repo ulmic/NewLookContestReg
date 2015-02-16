84ad8999c570b6074f1033f1d1851740b62310c2
pavel <kalashnikovisme@gmail.com>
2015-02-16 16:46:24 +0300

    fix deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,15 +24,23 @@ set :scm, "git"
 set :deploy_via, :remote_cache
 
 desc "Seed database data"
-task :seed_data do
-  run "cd #{current_path} && RAILS_ENV=#{rails_env} #{rake} db:seed"
-end
 
 namespace :assets do
   task :precompile do
     run "cd #{current_path} && RAILS_ENV=#{rails_env} #{rake} assets:precompile"
   end
 end
+
+namespace :db do
+  task :seed do
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} #{rake} db:seed"
+  end
+
+  task :migrate do
+    run "cd #{current_path} && RAILS_ENV=#{rails_env} #{rake} db:migrate"
+  end
+end
+before 'deploy:restart', 'db:migrate'
 before 'deploy:restart', 'assets:precompile'
 
 after "deploy:restart", "unicorn:restart"
