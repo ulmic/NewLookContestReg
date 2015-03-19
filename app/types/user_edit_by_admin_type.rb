986427ca2377bfee7da1130b81a9ef30a69a1d28
pavel <kalashnikovisme@gmail.com>
2015-03-19 19:03:05 +0300

    update admin users

--- app/types/user_edit_by_admin_type.rb
+++ app/types/user_edit_by_admin_type.rb
@@ -3,7 +3,7 @@ class UserEditByAdminType < User
 
   validates :email, presence: true
 
-  permit :email, :password, :avatar, :first_name, :last_name, :birth_date, :municipality, :patronymic, :school, :group, :mobile_phone,  :street, :city, :house, :building, :flat, :skype, :events, :honors, :media, :portfolio, :accept_agreement, :avatar_cache, :job, :role, :confirm_state
+  permit :email, :password, :avatar, :first_name, :last_name, :birth_date, :municipality, :patronymic, :school, :group, :mobile_phone,  :street, :city, :house, :building, :flat, :skype, :events, :honors, :media, :portfolio, :accept_agreement, :avatar_cache, :job, :role, :confirm_state, :creative_work, :url_creative_work
 
   def email=(email)
     write_attribute(:email, email.mb_chars.downcase)
