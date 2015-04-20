fde83d7e97594507688bbcfef643f24b3e1b4296
pavel <kalashnikovisme@gmail.com>
2015-04-21 00:06:49 +0300

    fix adds participants by admin

--- app/types/informat/participant_edit_by_admin_type.rb
+++ app/types/informat/participant_edit_by_admin_type.rb
@@ -3,7 +3,7 @@ class Informat::ParticipantEditByAdminType < Informat::Participant
 
   validates :email, presence: true
 
-  permit :email, :password, :avatar, :first_name, :last_name, :birth_date, :municipality, :patronymic, :school, :group, :mobile_phone,  :street, :city, :house, :building, :flat, :skype, :events, :honors, :media, :portfolio, :accept_agreement, :avatar_cache, :job, :role, :confirm_state, :creative_work, :url_creative_work
+  permit :email, :password, :avatar, :first_name, :last_name, :birth_date, :municipality, :patronymic, :school, :group, :mobile_phone,  :street, :city, :house, :building, :flat, :skype, :events, :honors, :media, :portfolio, :accept_agreement, :avatar_cache, :job, :role, :confirm_state, :creative_work, :url_creative_work, :activity_line
 
   def email=(email)
     write_attribute(:email, email.mb_chars.downcase)
